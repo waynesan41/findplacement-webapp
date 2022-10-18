5c24651659234829461b73735c046d993cf6b077
wayne acer <waynesan41@gmail.com>
2022-10-17 23:54:05 -0700

    mailer update

--- PhotoInventory/Backend/classes/Mail/Mailer.php
+++ PhotoInventory/Backend/classes/Mail/Mailer.php
@@ -23,15 +23,15 @@ class Mailer
       // $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
       $mail->SMTPDebug = 0;
       $mail->isSMTP(); //Send using SMTP
-      $mail->Host = "mail.717logistics.com"; //Set the SMTP server to send through
+      $mail->Host = "mail.privateemail.com"; //Set the SMTP server to send through
       $mail->SMTPAuth = true; //Enable SMTP authentication
-      $mail->Username = "wsan@sodecosolutions.com"; //SMTP username
-      $mail->Password = "aLy%c;ejIyzg"; //SMTP password
+      $mail->Username = "support@findplacement.org"; //SMTP username
+      $mail->Password = "xyz@mailJacker3.14!"; //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
       $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
       //Recipients
-      $mail->setFrom("wsan@sodecosolutions.com", "Reset Password");
+      $mail->setFrom("support@findplacement.org", "Reset Password");
       $mail->addAddress($email); //Add a recipient
       /* $mail->addAddress('ellen@example.com');               //Name is optional
     $mail->addReplyTo('info@example.com', 'Information');
